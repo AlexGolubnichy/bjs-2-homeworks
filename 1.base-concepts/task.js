3c22380b273d89885a7056057bd95821b170460c
AlexGolubnichy <alexandergolubnichy@yandex.ru>
2022-03-27 19:48:16 +0300

    save task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,23 @@
+'use strict';
 function solveEquation(a, b, c) {
-  let arr;
+ 
   // код для задачи №1 писать здесь
-  return arr; // array
+      let res = {};
+      let D = Math.sqrt(b) - 4 * a * c;
+      console.log( 'D = ' + D );
+      if( D < 0 ) 
+        return false;
+      res['discriminant'] = D;
+      if( D == 0 ) 
+        res["quadratic roots"] = (-b + Math.sqrt(D)) / (2 * a);
+      else if( D > 0 ) {
+        let tmp = [];
+        tmp.push((-b + Math.sqrt(D)) / (2 * a));
+        tmp.push((-b - Math.sqrt(D)) / (2 * a));
+        res["quadratic roots"]
+    }
+    
+  return res; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
